925effdc9d77660ab9aaaa39d2e8f939c4563cc6
Misael Villegas <misza@Misaels-MacBook-Pro.local>
2023-01-23 09:30:13 -0600

    init

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+    <q-btn color="primary" icon="check" label="OKs" @click="btnClick" />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { Notify } from "quasar";
+export default defineComponent({
+  name: "IndexPage",
+  methods: {
+    btnClick() {
+      Notify.create({ message: "done", icon: "announcement", type: "positive" });
+    },
+  },
+});
+</script>
